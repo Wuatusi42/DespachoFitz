242cc0696277377fd55f458d34a0815463f8b4d7
CesarLuis <cesarluissalazar30106@hotmail.com>
2024-11-13 18:58:55 -0600

    Correcciones 13/11/2024

--- src/main/java/com/indexia/DespachoFitz/Controller/TestController.java
+++ src/main/java/com/indexia/DespachoFitz/Controller/TestController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class TestController {
-    @GetMapping("/test")
+    @GetMapping("/index")
     public String test(Model model) {
         // Si estás usando un modelo, puedes agregar atributos si es necesario
         return "index";  // Asegúrate de que este archivo exista en templates
